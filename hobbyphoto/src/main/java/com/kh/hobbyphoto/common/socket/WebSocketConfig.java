e67249cf9d878b4f0bdd5ccf474806d72ebbd419
yoon <ge4621@naver.com>
2024-05-21 14:38:49 +0900

    .
    .

--- hobbyphoto/src/main/java/com/kh/hobbyphoto/common/socket/WebSocketConfig.java
+++ hobbyphoto/src/main/java/com/kh/hobbyphoto/common/socket/WebSocketConfig.java
@@ -1,16 +1,22 @@
 package com.kh.hobbyphoto.common.socket;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-public class WebSocketConfig implements WebSocketConfigurer{
-	@Autowired
-	private WebSockHandler sockHandler;
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
-	@Override
-	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(sockHandler, "/chat");
-	}
+    @Autowired
+    @Qualifier("webSockHandler")
+    private WebSockHandler sockHandler;
 
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(sockHandler, "/chat");
+    }
 }
